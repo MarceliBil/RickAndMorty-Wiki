c62bd4697af78cf2ee941dc59a956dd8a09a947b
MarceliBil <marbil003@gmail..com>
2022-01-07 03:29:20 +0100

    add copyright

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -2,8 +2,10 @@ import React from 'react'
 
 const Footer = () => {
     return (
-        <footer className='footer bg-dark d-flex align-items-center'>
-            <p className='w-100 text-center text-muted m-0'>&copy;2021. Marceli Bil.</p>
+        <footer className='footer py-4 bg-dark d-flex flex-column align-items-center justify-content-center'>
+            <p className='text-center '>&copy;2021. Marceli Bil.</p>
+
+            <p className='text-muted m-0'>Rick and Morty is created by Justin Roiland and Dan Harmon for <a href="https://www.adultswim.com" target="_blank" rel="nofollow noopener noreferrer" className='text-muted text-decoration-underline'>Adult Swim</a> . The data and images are used without claim of ownership and belong to their respective owners.</p>
         </footer>
     )
 }
